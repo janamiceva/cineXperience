10410803b3624e202e995c5b1917b88e39f94d4e
Jana Miceva <janamiceva>
2024-02-06 21:30:06 +0100

    Adding new method for deleting a reservation into the Reservation Reposiotry

--- server/src/repositories/reservations-repository.ts
+++ server/src/repositories/reservations-repository.ts
@@ -8,12 +8,13 @@ class ReservationsRepository {
         return reservations;
     }
 
-    public static async createReservation(userId: string, movieScreeningId: number, statusOfReservation: string): Promise<Reservation> {
+    public static async createReservation(userId: string, movieScreeningId: number, statusOfReservation: string, seats: number[]): Promise<Reservation> {
         const reservation = await prisma.reservation.create({
             data: {
                 userId,
                 movieScreeningId,
-                status: statusOfReservation
+                status: statusOfReservation,
+                seatsId: seats
             }
         })
         return reservation;
@@ -26,6 +27,30 @@ class ReservationsRepository {
             }
         })
         return reservations;
+    }
+
+    public static async deleteReservation(reservationId: number, seats: number[], movieScreeningId: number): Promise<Reservation> {
+        const movie = await prisma.$transaction(async (tx) => {
+            await tx.seatInMovieScreening.updateMany({
+                where: {
+                    movieScreeningId,
+                    seatId: {
+                        in: seats
+                    }
+                },
+                data: {
+                    status: false
+                }
+            })
+
+            const deleteReservation = await tx.reservation.delete({
+                where: {
+                    id: reservationId
+                }
+            })
+            return deleteReservation
+        })
+        return movie
     }
 }
 
