e080d8a128298883af5fc850f0d461cde685304e
Jana Miceva <janamiceva>
2024-02-06 21:20:53 +0100

    Changing the MovieScreening type

--- server/src/types/movieScreening.ts
+++ server/src/types/movieScreening.ts
@@ -2,7 +2,8 @@ type MovieScreening = {
     id: number,
     movieId: number,
     cinemaHallId: number,
-    price: number
+    price: number,
+    dateAndTimeOfScreening: Date
 }
 
 export default MovieScreening;
