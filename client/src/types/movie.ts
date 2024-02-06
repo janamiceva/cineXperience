96a75522e048342bfc5442089af13a9dc39229af
Jana Miceva <janamiceva>
2024-02-06 21:41:13 +0100

    Updating type for Movie

--- client/src/types/movie.ts
+++ client/src/types/movie.ts
@@ -1,13 +1,14 @@
-interface Movie {
+type Movie = {
     id: number;
     name: string;
     description: string;
     rating: number;
     director: string;
-    cast: string[];
+    cast: string[] ;
     duration: number;
     genre: string[];
     image: string;
+    videoId?: string
   };
   
   export default Movie;
