08cc1819db7cf13f889144da97b65884b49f126c
Jana Miceva <janamiceva>
2024-02-06 21:35:37 +0100

    Adding new routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,16 +6,20 @@ import CommentsRouter from './routes/comment-route'
 import HallsRouter from './routes/hall-route'
 import MovieScreeningsRouter from './routes/movie-screening-route'
 import ReservationRouter from './routes/reservation-route'
+import SeatRouter from './routes/seat-route'
+import SeatInMovieScreeningRouter from './routes/seat-in-movie-screening-route'
+import PaymentRouter from './routes/payment-route'
+
 
 const app = express()
 app.use(express.json())
 
 const corsOptions = {
-    origin: '*',
-    credentials: true,
-    optionSuccessStatus: 200
-  }
-  
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200
+}
+
 app.use(cors(corsOptions))
 
 app.use('/movies', MoviesRouter);
@@ -24,6 +28,9 @@ app.use('/comments', CommentsRouter);
 app.use('/halls', HallsRouter);
 app.use('/movieScreenings', MovieScreeningsRouter);
 app.use('/reservations', ReservationRouter)
+app.use('/seats', SeatRouter)
+app.use('/payment', PaymentRouter)
+app.use('/seatsInMovieScreening', SeatInMovieScreeningRouter)
 
 
-app.listen(3001, () => {console.log('server started ....')})
+app.listen(3001, () => { console.log('server started ....') })
