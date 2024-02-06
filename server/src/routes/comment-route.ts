2e8ac4df73e84d325d9f6709e58e14f5d65e9e24
Jana Miceva <janamiceva>
2024-02-06 21:27:12 +0100

    Updating the comment routes

--- server/src/routes/comment-route.ts
+++ server/src/routes/comment-route.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import CommentController from "../controllers/comments-controller";
+import isAuthenticatedMiddleware from "../../middlewares/auth-middleware"
+
 
 const router = express.Router();
 
-router.post('/:movieId', CommentController.createComment);
-router.get('/', CommentController.getAllComments);
-router.get('/:movieId', CommentController.getAllCommentsByMovie);
-router.patch('/:commentId', CommentController.editCommentForMovie)
-router.delete('/:commentId', CommentController.deleteComment)
+router.get('/', isAuthenticatedMiddleware, CommentController.getAllComments);
+router.get('/:movieId', isAuthenticatedMiddleware, CommentController.getAllCommentsByMovie);
+
+router.post('/:movieId', isAuthenticatedMiddleware,  CommentController.createComment);
+
+router.patch('/:commentId', isAuthenticatedMiddleware, CommentController.editCommentForMovie)
+
+router.delete('/:commentId', isAuthenticatedMiddleware, CommentController.deleteComment)
 
 export default router
