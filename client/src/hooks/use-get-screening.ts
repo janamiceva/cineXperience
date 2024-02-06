584fcacb0af17f666825916109277a3c544603d3
Jana Miceva <janamiceva>
2024-02-06 21:45:47 +0100

    Updating the useGetScreening hook

--- client/src/hooks/use-get-screening.ts
+++ client/src/hooks/use-get-screening.ts
@@ -1,12 +1,12 @@
 import { useQuery } from "react-query";
 import { axiosIntance } from "../interceptors/auth-interceptor";
 
-const getScreening = async (screeningId: number) => {
+const getScreening = async (screeningId: string) => {
     const screening = await axiosIntance.get(`/movieScreenings/${screeningId}`)
     return screening.data
 }
 
-const useGetScreening = (screeningId: number) => {
+const useGetScreening = (screeningId: string) => {
 
     return useQuery(['screening', screeningId], () => getScreening(screeningId))
 }
