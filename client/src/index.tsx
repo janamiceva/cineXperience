d283fb437661a97de68149ab44ce6ebae8de02d8
Jana Miceva <janamiceva>
2024-02-06 21:54:10 +0100

    Adding a Stripe Provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { Elements, StripeProvider } from 'react-stripe-elements'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter } from 'react-router-dom'
 import { SnackbarProvider } from 'notistack'
@@ -27,13 +28,17 @@ const theme = createTheme();
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <SnackbarProvider autoHideDuration={3000} maxSnack={2}>
-        <BrowserRouter>
-          <QueryClientProvider client={queryClient}>
-            <App />
-          </QueryClientProvider>
-        </BrowserRouter>
-      </SnackbarProvider>
+      <StripeProvider apiKey='pk_test_51OLomxHipg0mTMHOBFbn5MDbvBUR929ONILzXr5OkOWLXmuEvGc9nFLBX7f09LLlefHRtBme3gYXfEmlte1xCB9y00xS77GSjl'>
+        <Elements>
+          <SnackbarProvider autoHideDuration={3000} maxSnack={2}>
+            <BrowserRouter>
+              <QueryClientProvider client={queryClient}>
+                <App />
+              </QueryClientProvider>
+            </BrowserRouter>
+          </SnackbarProvider>
+        </Elements>
+      </StripeProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
