d9c0308f5652c7d88b04c891f023a60d85ac3ab9
Jana Miceva <janamiceva>
2024-02-06 21:42:25 +0100

    Adding type for MovieInput

---
+++ client/src/types/movie-input.ts
@@ -0,0 +1,12 @@
+export type MovieInput = {
+    movieId?: number,
+    name?: string,
+    genre?: string[],
+    description?: string,
+    director?: string,
+    duration?: number,
+    image?: string,
+    rating?: number,
+    cast?: string[],
+    videoId?: string
+}
