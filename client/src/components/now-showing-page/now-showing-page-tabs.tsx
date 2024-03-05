6fc36ab2d97307aa524446c1c81034885f88e58e
Jana Miceva <janamiceva>
2024-03-05 21:42:54 +0100

    Changing the content in Now Showing Page Tabs

--- client/src/components/now-showing-page/now-showing-page-tabs.tsx
+++ client/src/components/now-showing-page/now-showing-page-tabs.tsx
@@ -2,15 +2,16 @@ import Movie from "../../types/movie";
 import MovieCard from "./movie-card";
 import useGetMovieByGenre from "../../hooks/use-get-movie-by-genre";
 import AllMoviesContent from "./all-movies-content";
+import { Box, Typography } from "@mui/material";
 
 function getNowShowingPageTabs() {
 
     const { data: romanceMovies } = useGetMovieByGenre('Romance');
     const { data: dramaMovies } = useGetMovieByGenre('Drama');
-    const { data: scienceFictionMovies } = useGetMovieByGenre('Science Fiction');
+    const { data: comedyMovies } = useGetMovieByGenre('Comedy');
     const { data: actionMovies } = useGetMovieByGenre('Action');
     const { data: adventureMovies } = useGetMovieByGenre('Adventure');
-    const { data: thrillerMovies } = useGetMovieByGenre('Thriller');
+    const { data: animationMovies } = useGetMovieByGenre('Animation');
     const { data: crimeMovies } = useGetMovieByGenre('Crime');
 
 
@@ -23,9 +24,9 @@ function getNowShowingPageTabs() {
         {
             label: 'Romance',
             value: '2',
-            content: romanceMovies?.map((movie: Movie) => (
+            content: romanceMovies?.length ? romanceMovies?.map((movie: Movie) => (
                 <MovieCard key={movie.id} movieId={movie.id} />
-            ))
+            )) : <Typography variant="h4" sx={{color: 'white', marginTop: '50px'}}>No movies found with this genre</Typography> 
         },
         {
             label: 'Drama',
@@ -35,9 +36,9 @@ function getNowShowingPageTabs() {
             ))
         },
         {
-            label: 'Sci-Fi',
+            label: 'Comedy',
             value: '4',
-            content: scienceFictionMovies?.map((movie: Movie) => (
+            content: comedyMovies?.map((movie: Movie) => (
                 <MovieCard key={movie.id} movieId={movie.id} />
             ))
         },
@@ -56,9 +57,9 @@ function getNowShowingPageTabs() {
             ))
         },
         {
-            label: 'Thriller',
+            label: 'Animation',
             value: '7',
-            content: thrillerMovies?.map((movie: Movie) => (
+            content: animationMovies?.map((movie: Movie) => (
                 <MovieCard key={movie.id} movieId={movie.id} />
             ))
         },
