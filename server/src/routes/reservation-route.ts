9570f73cb32fcb2b310ed2376c35470bbbc1033d
Jana Miceva <janamiceva>
2024-03-05 21:33:54 +0100

    Adding hasRole middleware on getAllReservations route

--- server/src/routes/reservation-route.ts
+++ server/src/routes/reservation-route.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import ReservationsController from "../controllers/reservations-controller";
 import isAuthenticatedMiddleware from "../../middlewares/auth-middleware";
+import UserRole from "../types/user-role";
+import hasRohasRoleMiddlewarele from "../../middlewares/role-middleware";
 
 const router = express.Router()
 
-router.get('/', isAuthenticatedMiddleware, ReservationsController.getAllReservations)
+router.get('/', isAuthenticatedMiddleware, hasRohasRoleMiddlewarele(UserRole.admin), ReservationsController.getAllReservations)
 router.get('/:userId', isAuthenticatedMiddleware, ReservationsController.getAllReservationsForUser)
 
 router.post('/create/:movieScreeningId', isAuthenticatedMiddleware, ReservationsController.createReservation)
